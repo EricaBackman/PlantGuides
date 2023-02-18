4a11a6565ed3e0e058a6ffea196712fea55a0ee7
Erica Bäckman <112472987+EricaBackman@users.noreply.github.com>
2023-02-18 14:49:46 +0100

    Setup footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
-import NavBar from './components/NavBar.vue'
+import NavBar from './components/NavBar.vue';
 import NewsForm from './components/NewsForm.vue';
+import PlantFooter from './components/PlantFooter.vue';
 </script>
 
 <template>
   <NavBar />
   <NewsForm />
+  <PlantFooter />
 </template>
 
 <style scoped>
---
+++ src/components/PlantFooter.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="footer-wrapper">
+            <h5>Contact us</h5>
+            <p>Plantstreet 123</p>
+            <p>123 45 Greenwill</p>
+            <p><a href="mailto:greenplants@info.com">greenplants@info.com</a></p>
+    </div>
+
+</template>
+
+<style scoped>
+.footer-wrapper {
+    background-color: rgb(77, 93, 81);
+    color: white;
+    margin: 0;
+    padding: 10vw;
+}
+
+.footer-wrapper p {
+    margin: 0;
+}
+
+.footer-wrapper a {
+    color: white;
+}
+
+.footer-wrapper a:hover {
+    color: black;
+}
+</style>
