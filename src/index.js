036d1afcfaa29120a60600898325c06da22aba9a
Erica Bäckman <112472987+EricaBackman@users.noreply.github.com>
2023-11-21 22:18:08 +0100

    move index.css to src

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import router from './router'
 import store from './store'
 import 'bootstrap/dist/css/bootstrap.css'
 import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
-import './assets/index.css'
+import './index.css'
 
 
 const app = createApp(App)
