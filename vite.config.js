ead3a8350d1457a6f1726a33b6a7698d680c1736
Erica Bäckman <112472987+EricaBackman@users.noreply.github.com>
2023-11-21 22:45:24 +0100

    change base in vite config

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    base: '/index.html',
+    base: '/plantguides',
   plugins: [vue()],
   resolve: {
     alias: {
