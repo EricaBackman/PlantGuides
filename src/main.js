e18cea3e50d622b0d7961df6c78d6f1d3a0ef078
Erica Bäckman <112472987+EricaBackman@users.noreply.github.com>
2023-11-21 22:59:17 +0100

    change in main.js

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router'
+import router from '.'
 import store from './store'
 import 'bootstrap/dist/css/bootstrap.css'
 import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
