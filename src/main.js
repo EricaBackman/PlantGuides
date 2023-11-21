a5561e50228d3a80b7cb61237e42b1a5e83eddfc
Erica Bäckman <112472987+EricaBackman@users.noreply.github.com>
2023-11-21 23:38:04 +0100

    move index.js to src

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import router from './router/index'
+import router from './index'
 import store from './store'
 import 'bootstrap/dist/css/bootstrap.css'
 import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js'
